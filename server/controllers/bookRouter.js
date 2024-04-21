b7726dc7e2e8a29c5453d6b10f47e1820b18a051
cSahu14 <sahuchidananda1999@gmail.com>
2024-04-21 12:07:16 +0530

    first commit

---
+++ server/controllers/bookRouter.js
@@ -0,0 +1,23 @@
+import createHttpError from "http-errors";
+import bookModel from "../models/bookModel.js"
+
+const books = async (req, res, next) => {
+
+    let bookData;
+    try {
+
+        bookData = await bookModel.find({})
+        if (!bookData) {
+            return next(createHttpError(400, "Book not found."))
+        }
+    } catch (error) {
+        console.log("error", error);
+        return next(createHttpError(400, "Error while getting book data"))
+    }
+
+    return res.status(200).json({
+        message: bookData
+    })
+}
+
+export { books }
